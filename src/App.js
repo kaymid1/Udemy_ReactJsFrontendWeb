53be37781c1b8a098307a09d5aac68a1fd21e838
kaymid <dquanht0@gmail.com>
2022-09-29 00:18:55 +0700

    Adding npx react-codemod rename-unsafe-lifecycles => to enable componentWillMount method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { Component} from 'react';
+import axios from 'axios';
+class App extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        //state 
+        this.state = {
+            user: []
+        }
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //api call
+    componentWillMount(){
+        axios('https://randomuser.me/api/').
+        then(response => console.log(response));
+    }
+
+    render(){
+        return <div className="App">We will be back!</div>;
+    }
 }
 
 export default App;
